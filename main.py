05360691e7769c99bd1386d07f9627766a3046f9
shahmananv13 <shah.manan.vinod@gmail.com>
2023-05-05 14:26:27 +0530

    Added database contact message capturing

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/new-bie'
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+	sno = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(50), nullable=False)
+	name = db.Column(db.String(50), nullable=False)
+	phone_num = db.Column(db.String(20), nullable=False)
+	mes = db.Column(db.String(50), nullable=False)
+	date = db.Column(db.String(50), nullable = True)
+
 
 @app.route("/")
 def home():
@@ -9,6 +24,7 @@ def home():
 def about():
     return render_template('about.html')
 
+
 @app.route("/index")
 def index():
     return render_template('index.html')
@@ -17,8 +33,17 @@ def index():
 def post():
     return render_template('post.html')
 
-@app.route("/contact")
+
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+	if request.method == 'POST':
+		email = request.form.get('email')
+		name = request.form.get('name')
+		msg = request.form.get('msg')
+		phone = request.form.get('phone')
+		entry = Contact(name = name, mes = msg, date = datetime.now(), phone_num = phone, email = email)
+		db.session.add(entry)
+		db.session.commit()
+	return render_template('contact.html')
 
 app.run(debug=True)
